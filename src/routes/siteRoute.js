1c8502f150d738ccad97be106d5af87c2e4aeacd
Aditya Kumar Singh <adityakumarsingh09@gmail.com>
2025-01-26 17:50:41 +0530

    changes done

--- src/routes/siteRoute.js
+++ src/routes/siteRoute.js
@@ -2,17 +2,16 @@ const app = require("express")();
 
 const {
   create,
-  getWorkOrders,
-  getWorkOrderById,
-  UpdateWorkOrderById,
-  deleteWorkOrderById,
-} = require("../controllers/WorkOrderController");
-
+  getSites,
+  getSiteById,
+  UpdateSiteById,
+  deleteSiteById,
+} = require("../controllers/siteController");
 
 app.post("/create", create);
-app.post("/get-Work_orders", getWorkOrders);
-app.post("/get-Work_order-by-id", getWorkOrderById);
-app.post("/update-Work_order-by-id/:id", UpdateWorkOrderById);
-app.post("/delete-Work_order-by-id", deleteWorkOrderById);
+app.post("/get-sites", getSites);
+app.post("/get-site-by-id", getSiteById);
+app.post("/update-site-by-id/:id", UpdateSiteById);
+app.post("/delete-site-by-id", deleteSiteById);
 
 module.exports = app;
