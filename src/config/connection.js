156a1b707977d491f7427a13389507a235ebbdef
Aditya Singh <adityakumarsingh09@gmail.com>
2026-01-04 16:42:09 +0530

    mongoose connection

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,9 +1,27 @@
+// const mongoose = require("mongoose");
+
+// mongoose.set("strictQuery", false);
+// mongoose.set("strictPopulate", false);
+
+// mongoose
+//   .connect(process.env.DB_URI, { useNewUrlParser: true })
+//   .then(() => console.log(`Database connected on ${process.env.DB_URI}`))
+//   .catch((err) => console.error(err));
+
+
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 mongoose.set("strictPopulate", false);
 
-mongoose
-  .connect(process.env.DB_URI, { useNewUrlParser: true })
-  .then(() => console.log(`Database connected on ${process.env.DB_URI}`))
-  .catch((err) => console.error(err));
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log("✅ MongoDB connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
